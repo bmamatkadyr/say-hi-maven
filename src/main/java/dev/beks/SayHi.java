cef6b331f10de82ee17ee7479477cbb815c5a814
bmamatkadyr <beksultancs@gmail.com>
2024-09-20 00:44:47 +0600

    Initial commit

---
+++ src/main/java/dev/beks/SayHi.java
@@ -0,0 +1,13 @@
+package dev.beks;
+
+public class SayHi {
+
+    public static void sayHi() {
+        System.out.println("Hi!");
+    }
+
+    public static void sayHi(String name) {
+        System.out.println("Hi, " + name + "!");
+    }
+
+}
